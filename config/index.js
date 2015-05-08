e32a6b37c7c112081772a228d476ca21d56b5ec0
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-05-08 21:42:57 +0200

    Initial commit

---
+++ config/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var config = {
+  mongoDBin: 'mongodb://localhost:27017/tfk',
+  mongoDBout: 'mongodb://localhost:27017/vkt',
+  filePath: ''
+};
+
+module.exports = config;
---
+++ test/testSetStatus.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var assert = require('assert');
+var setStatus = require('../lib/setStatus');
+
+describe('setStatus', function() {
+
+  it('it requires an item object', function(done) {
+
+    var item = false;
+
+    setStatus(item, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required input: item object/.test(err)) {
+            return true;
+          }
+        },
+        'Unexpected error'
+      );
+      done();
+    });
+  });
+
+  it('it requires item.sokegrunnlag to exists', function(done) {
+
+    var item = {
+      sokegrunnlag: false
+    };
+
+    setStatus(item, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required param: item.sokegrunnlag/.test(err)) {
+            return true;
+          }
+        },
+        'Unexpected error'
+      );
+      done();
+    });
+  });
+
+  it('it requires item.measuredDistanceRegisteredAddress to exists', function(done) {
+
+    var item = {
+      sokegrunnlag: true,
+      measuredDistanceRegisteredAddress: false
+    };
+
+    setStatus(item, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required param: item.measuredDistanceRegisteredAddress/.test(err)) {
+            return true;
+          }
+        },
+        'Unexpected error'
+      );
+      done();
+    });
+  });
+
+  it('it sets item.behandlingsStatus to Automatic given the right input', function(done) {
+
+    var item = require('./data/automatic_yes.json');
+
+    setStatus(item, function(err, data) {
+      if (err) {
+        console.error(err);
+      } else {
+        assert.equal(data.behandlingsStatus, 'Automatic');
+      }
+      done();
+    });
+  });
+
+});
