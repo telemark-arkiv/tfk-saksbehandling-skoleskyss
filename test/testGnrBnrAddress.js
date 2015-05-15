9e45039974b5fbc14075a6a4eb6b180ad2c01ffd
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-05-16 00:08:32 +0200

    Implements getGnrBnrAddress

---
+++ test/testGnrBnrAddress.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var assert = require('assert');
+var getGnrBnrAddress = require('../lib/getGnrBnrAddress');
+
+describe('getGnrBnrAddress', function() {
+
+  it('it requires a querystring to exist', function(done) {
+
+    var querystring = false;
+
+    getGnrBnrAddress(querystring, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required param: querystring/.test(err)) {
+            return true;
+          }
+        },
+        'Unexpected error'
+      );
+      done();
+    });
+  });
+
+  it('it returns data as expected', function(done) {
+
+    var querystring = '0806-60/77';
+
+    getGnrBnrAddress(querystring, function(err, data) {
+      if (err) {
+        throw err;
+      } else {
+        assert.equal(data.geocoded.lat, 59.21585817);
+        assert.equal(data.geocoded.lng, 9.61178004);
+      }
+      done();
+    });
+  });
+
+});
