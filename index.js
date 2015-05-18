db7c390fc0ac43c22e3e89e8e132db661d1ecb7b
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-05-18 17:57:35 +0200

    Implements final methods

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 'use strict';
 
-function checkStatuses(item, callback){
+function doSaksbehandling(item, callback) {
+  var fixAddressMeasureDistance = require('./lib/fixAddressMeasureDistance');
+  var setBehandlingsType = require('./lib/setBehandlingstype');
+
   if (!item) {
     return callback(new Error('Missing required input: item object'), null);
   }
 
-  return callback(null, item);
+  fixAddressMeasureDistance(item, function(error, data){
+    if (error) {
+      return callback(error, null);
+    } else {
+      setBehandlingsType(data, function(err, result){
+        if (err) {
+          return callback(err, null);
+        } else {
+          return callback(null, result);
+        }
+      });
+    }
+  });
 
 }
 
-module.exports = checkStatuses;
+module.exports = doSaksbehandling;
