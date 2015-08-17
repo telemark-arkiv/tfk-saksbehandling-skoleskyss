6fd51c6a6b054407741e8f10168b46fa40c07f5e
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-08-17 20:19:56 +0200

    Adds test for lookupSeeiendom

---
+++ test/lookupSeeiendom-test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var assert = require('assert');
+var thru = require('thru');
+var pipeline = thru();
+var lookupSeeiendom = require('../lib/lookupSeeiendom');
+var testData = require('./data/lookupSeeiendom-testdata.json');
+
+
+describe('lookupSeeiendom', function() {
+
+  it('it returns expected result', function(done) {
+
+    var testResult = thru(function(itemString, callback) {
+      var item = JSON.parse(itemString);
+      assert.equal(item.registeredAddressGeocoded, testData.expectedResult);
+      done()
+      return callback(null, 'Success');
+    });
+
+    pipeline
+      .pipe(lookupSeeiendom)
+      .pipe(testResult);
+
+    pipeline.write(JSON.stringify(testData.item));
+
+  });
+
+});
