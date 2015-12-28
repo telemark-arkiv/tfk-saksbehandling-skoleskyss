709820e412e9519bb4d78b0ab394f7a217bfcd95
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-12-28 14:20:21 +0100

    Updates test to tap

--- test/testMeasureDistance.js
+++ test/testMeasureDistance.js
@@ -1,90 +1,50 @@
 'use strict'
 
-var assert = require('assert')
+var tap = require('tap')
 var measureDistance = require('../lib/measureDistance')
 
-describe('measureDistance', function () {
-  it('it requires an options object', function (done) {
-    var options = false
-
-    measureDistance(options, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required input: options/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('it requires an options object', function (test) {
+  var options = false
+  var expectedErrorMessage = 'Missing required input: options'
+  measureDistance(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('it requires options.origin to exists', function (done) {
-    var options = {
-      origin: false
-    }
-
-    measureDistance(options, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required input: options.origin/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('it requires options.origin to exists', function (test) {
+  var options = {
+    origin: false
+  }
+  var expectedErrorMessage = 'Missing required input: options.origin'
+  measureDistance(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('it requires options.destination to exists', function (done) {
-    var options = {
-      origin: true,
-      destination: false
-    }
-
-    measureDistance(options, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required input: options.destination/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('it requires options.destination to exists', function (test) {
+  var options = {
+    origin: true,
+    destination: false
+  }
+  var expectedErrorMessage = 'Missing required input: options.destination'
+  measureDistance(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('it measures the walking distance', function (done) {
-    var options = {
-      origin: 'Lisleherad skole, 3680 Notodden',
-      destination: 'Kjærlighetsstien 24, Notodden'
+tap.test('it measures the walking distance', function (test) {
+  var options = {
+    origin: 'Bjarne Hansens vei 22, 3680 Notodden',
+    destination: 'Kjærlighetsstien 24, 3681 Notodden'
+  }
+  measureDistance(options, function (error, data) {
+    if (error) {
+      throw error
     }
-
-    measureDistance(options, function (err, data) {
-      if (err) {
-        console.error(err)
-      } else {
-        assert.equal(data.distanceValue, 6464)
-      }
-      done()
-    })
+    tap.equal(data.distanceValue, 6051, 'Length 6051')
+    test.done()
   })
 })
