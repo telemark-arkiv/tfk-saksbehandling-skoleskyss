c8525f060bbbf620ceafb704cd9d3ea22d3d8f81
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-12-28 13:41:57 +0100

    Updates test to tap

--- test/lookupSeeiendom-test.js
+++ test/lookupSeeiendom-test.js
@@ -1,24 +1,22 @@
 'use strict'
 
-var assert = require('assert')
+var tap = require('tap')
 var thru = require('thru')
 var pipeline = thru()
 var lookupSeeiendom = require('../lib/lookupSeeiendom')
 var testData = require('./data/lookupSeeiendom-testdata.json')
 
-describe('lookupSeeiendom', function () {
-  it('it returns expected result', function (done) {
-    var testResult = thru(function (itemString, callback) {
-      var item = JSON.parse(itemString)
-      assert.equal(item.registeredAddressGeocoded, testData.expectedResult)
-      done()
-      return callback(null, 'Success')
-    })
-
-    pipeline
-      .pipe(lookupSeeiendom)
-      .pipe(testResult)
-
-    pipeline.write(JSON.stringify(testData.item))
+tap.test('it returns expected result', function (test) {
+  var testResult = thru(function (itemString, callback) {
+    var item = JSON.parse(itemString)
+    tap.equal(item.registeredAddressGeocoded, testData.expectedResult)
+    test.done()
+    return callback(null, 'Success')
   })
+
+  pipeline
+    .pipe(lookupSeeiendom)
+    .pipe(testResult)
+
+  pipeline.write(JSON.stringify(testData.item))
 })
