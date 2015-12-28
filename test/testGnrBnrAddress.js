435a2b188fc78157d19390e3d44c34c99ed3f340
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-12-28 14:07:23 +0100

    Updates test to tap

--- test/testGnrBnrAddress.js
+++ test/testGnrBnrAddress.js
@@ -1,41 +1,25 @@
 'use strict'
 
-var assert = require('assert')
+var tap = require('tap')
 var getGnrBnrAddress = require('../lib/getGnrBnrAddress')
 
-describe('getGnrBnrAddress', function () {
-  it('it requires a querystring to exist', function (done) {
-    var querystring = false
-
-    getGnrBnrAddress(querystring, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required param: querystring/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('it requires a querystring to exist', function (test) {
+  var querystring = false
+  var expectedErrorMessage = 'Missing required param: querystring'
+  getGnrBnrAddress(querystring, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('it returns data as expected', function (done) {
-    var querystring = '0806-60/77'
-
-    getGnrBnrAddress(querystring, function (err, data) {
-      if (err) {
-        throw err
-      } else {
-        assert.equal(data.geocoded.lat, 59.21585817)
-        assert.equal(data.geocoded.lng, 9.61178004)
-      }
-      done()
-    })
+tap.test('it returns data as expected', function (test) {
+  var querystring = '0806-60/77'
+  getGnrBnrAddress(querystring, function (error, data) {
+    if (error) {
+      throw error
+    }
+    tap.equal(data.geocoded.lat, 59.21585817, 'lat OK')
+    tap.equal(data.geocoded.lng, 9.61178004, 'lng OK')
+    test.done()
   })
 })
